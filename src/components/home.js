38447c8564bfc696d0d81fac7ef2551b6f3eaf36
anasvassia <alamothe@college.harvard.edu>
2023-08-26 22:01:04 -0400

    bank png moved

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import bank from "./bank.png";
 
 function Home() {
   return (
@@ -9,7 +10,7 @@ function Home() {
       header="BadBank Landing Page"
       title="Welcome to the bank"
       text="You can use this bank"
-      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
+      body={<img src={bank} className="img-fluid" alt="Responsive image" />}
     />
   );
 }
