c7588d1786077a2248f74ef0135f6cbe62c5b39e
Anastasia Lamothe <aelamothe@gmail.com>
2023-09-17 21:58:45 -0400

    back to working!

--- src/App.js
+++ src/App.js
@@ -10,39 +10,29 @@ import Home from "./components/home";
 import Login from "./components/login";
 import NavBar from "./components/navbar";
 import Withdraw from "./components/withdraw";
-import { gapi } from "gapi-script";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const client_id =
   "614440106254-50kj27efhlrkmamgvg3vesvr4pk83cdr.apps.googleusercontent.com";
 
 function App() {
-  // initialize client
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        client_id: client_id,
-        scope: "",
-      });
-    }
-
-    gapi.load("client:auth2", start);
-  });
-
   // shared context is our set of users
   return (
-    <HashRouter>
-      <CurrentUser.Provider value={{ currUser: "" }}>
-        <NavBar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/CreateAccount" element={<CreateAccount />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/deposit" element={<Deposit />} />
-          <Route path="/withdraw" element={<Withdraw />} />
-          <Route path="/alldata" element={<AllData />} />
-        </Routes>
-      </CurrentUser.Provider>
-    </HashRouter>
+    <GoogleOAuthProvider clientId={client_id}>
+      <HashRouter>
+        <CurrentUser.Provider value={{ currUser: "" }}>
+          <NavBar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/CreateAccount" element={<CreateAccount />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/deposit" element={<Deposit />} />
+            <Route path="/withdraw" element={<Withdraw />} />
+            <Route path="/alldata" element={<AllData />} />
+          </Routes>
+        </CurrentUser.Provider>
+      </HashRouter>
+    </GoogleOAuthProvider>
   );
 }
 
